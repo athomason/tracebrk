19298d9b2d0564b09ae214b8ceafd5a820637196
Adam Thomason <ad@mthomason.net>
2013-06-19 20:12:33 -0700

    Use addr2line if an image is provided

--- grow.c
+++ grow.c
@@ -1,4 +1,7 @@
-// test utility to call malloc repeatedly
+// test utility to call sbrk/malloc repeatedly
+
+#define USE_MALLOC 1
+#define USE_SBRK 0
 
 #include <malloc.h>
 #include <stdio.h>
@@ -7,22 +10,7 @@
 
 const int PAGE_SIZE = 4 * 1024;
 
-int main(int argc, char** argv) {
-    long startup = 2;
-    long bytes = 1024;
-    int secs = 1;
-
-    // never use malloc, we're looking for brk calls
-    if (!mallopt(M_MMAP_THRESHOLD, bytes*2))
-	perror("mallopt failed");
-
-    if (argc > 1)
-        bytes = atol(argv[1]);
-    if (argc > 2)
-        secs = atoi(argv[1]);
-
-    pid_t pid = getpid();
-
+static inline long data_size() {
     FILE* f;
     if ((f = fopen("/proc/self/statm", "r")) == NULL) {
         perror("couldn't read /proc/self/statm");
@@ -31,28 +19,57 @@ int main(int argc, char** argv) {
 
     long pages;
     if (fscanf(f, "%*d %*d %*d %*d %*d %ld\n", &pages))
-	fprintf(stderr, "[%d] brk=%p data=%ld\n", pid, sbrk(0), pages * PAGE_SIZE);
+        return pages * PAGE_SIZE;
+    fclose(f);
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    long startup = 2;
+    long bytes = 1024;
+    int secs = 1;
+
+    if (argc > 1)
+        bytes = atol(argv[1]);
+    if (argc > 2)
+        secs = atoi(argv[1]);
+
+    // always want brk allocations
+    mallopt(M_MMAP_THRESHOLD, bytes << 2);
+
+    pid_t pid = getpid();
+
+    fprintf(stderr, "[%d] brk=%p data=%ld\n", pid, sbrk(0), data_size());
 
     sleep(startup);
 
     long total = 0;
     for (;;) {
-        fprintf(stderr, "[%d] ", pid);
+        fprintf(stderr, "[%d]", pid);
+        #if USE_MALLOC
         void* p;
-        if ((p = malloc(bytes)) == NULL) {
-            fprintf(stderr, "malloc failed");
+        if (!(p = malloc(bytes))) {
+            fprintf(stderr, " malloc(failed),");
         }
         else {
             total += bytes;
-            fprintf(stderr, "malloc %ld @ %p", bytes, p);
+            fprintf(stderr, " malloc(%ld)@%p,", bytes, p);
         }
+        #endif
+        #if USE_SBRK
+        if (!sbrk(bytes)) {
+            fprintf(stderr, " sbrk(failed),");
+        }
+        else {
+            total += bytes;
+            fprintf(stderr, " sbrk(%ld),", bytes);
+        }
+        #endif
 
-        fprintf(stderr, ", total=%ld", total);
-        fprintf(stderr, ", brk=%p", sbrk(0));
-
-        rewind(f);
-        if (fscanf(f, "%*d %*d %*d %*d %*d %ld\n", &pages))
-            fprintf(stderr, ", data=%ld\n", pages * PAGE_SIZE);
+        fprintf(stderr, " total=%ld", total);
+        fprintf(stderr, " brk=%p", sbrk(0));
+        fprintf(stderr, " data=%ld", data_size());
+        fprintf(stderr, "\n");
 
         sleep(secs);
     }
--- tracebrk.c
+++ tracebrk.c
@@ -4,15 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/ptrace.h>
+#include <sys/reg.h>
 #include <sys/syscall.h>
 #include <sys/user.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #if defined __x86_64__
-    #define SYSCALL_NUM orig_rax
+    // http://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI
+    // http://www.x86-64.org/documentation/abi.pdf
+    #define SYSCALL_ARG1        rdi
+    #define SYSCALL_ARG2        rsi
+    #define SYSCALL_ARG3        rdx
+    #define SYSCALL_ARG4        r10
+    #define SYSCALL_ARG5        r8
+    #define SYSCALL_ARG6        r9
+    #define SYSCALL_NUM_OFFSET  (8*ORIG_RAX)
+    #define SYSCALL_RET_OFFSET  (8*RAX)
 #else
-    #error Only x86_64 is supported.
+    #error Only x86_64 supported.
 #endif
 
 
@@ -20,17 +32,28 @@
 #define FATAL(...) do { fprintf(stderr, __VA_ARGS__); return EXIT_FAILURE; } while (0)
 #define PANIC(...) do { fprintf(stderr, __VA_ARGS__); goto CLEANUP; } while (0)
 
+int stopped = 0;
+void handler(int sig) {
+    if (sig == SIGINT)
+        if (stopped++)
+            _exit(-1);
+}
+
 int
 main(int argc, char** argv)
 {
     if (argc < 2)
-        FATAL("usage: %s <pid>\n", argv[0]);
+        FATAL("usage: %s <pid> [<exe>]\n", argv[0]);
 
     pid_t pid = atoi(argv[1]);
     if (!pid)
         FATAL("bad pid %d\n", pid);
     if (kill(pid, 0) < 0)
         FATAL("couldn't signal process %d: %s\n", pid, strerror(errno));
+
+    const char* image = NULL;
+    if (argc > 2)
+        image = argv[2];
 
     unw_addr_space_t as = unw_create_addr_space(&_UPT_accessors, 0);
     if (!as)
@@ -42,9 +65,9 @@ main(int argc, char** argv)
     if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) < 0)
         FATAL("PTRACE_ATTACH failed: %s\n", strerror(errno));
 
-    // go through loop twice per syscall, once on entry and again on exit
-    int state, interesting;
-    for (state = 1; ; state ^= 1) {
+    int entering; // go through loop twice per syscall, once on entry and again on exit
+    void* last_brk = NULL;
+    for (entering = 1; !stopped; entering = !entering) {
         int status;
         if (waitpid(-1, &status, 0) < 0) {
             if (errno == EINTR)
@@ -55,25 +78,53 @@ main(int argc, char** argv)
         if (WIFEXITED(status) || WIFSIGNALED(status))
             break;
 
-        int pending_sig = 0;
-        if (WIFSTOPPED(status) && WSTOPSIG(status) != SIGTRAP) {
-            pending_sig = WSTOPSIG(status);
-            if (WSTOPSIG(status) == SIGKILL)
+        int pending_sig = 0, show_backtrace = 0;
+        if (WIFSTOPPED(status)) {
+            int sig = WSTOPSIG(status);
+            if (sig == SIGSTOP)
+                entering = 0;
+            else if (sig == SIGKILL)
                 break;
+            else if (sig != SIGTRAP)
+                pending_sig = sig;
+            else { // SIGTRAP
+                long syscall, retval;
+                struct user_regs_struct regs;
+                syscall = ptrace(PTRACE_PEEKUSER, pid, SYSCALL_NUM_OFFSET, NULL);
+                retval = ptrace(PTRACE_PEEKUSER, pid, SYSCALL_RET_OFFSET, NULL);
+                ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+
+                if (!entering) {
+                    if (syscall == SYS_brk) {
+                        void* new_brk = (void*) retval;
+                        if (last_brk == NULL)
+                            printf("brk is at %p\n", new_brk);
+                        else if (new_brk == last_brk)
+                            printf("brk unchanged at %p\n", new_brk);
+                        else
+                            printf("new brk is at %p (%ld bytes more)\n", new_brk, new_brk - last_brk);
+                        last_brk = new_brk;
+                        show_backtrace = 1;
+                    }
+                    else if (syscall == SYS_mmap) {
+                        void* addr = (void*) retval;
+                        int len = (int) regs.SYSCALL_ARG2;
+                        int prot = (int) regs.SYSCALL_ARG3;
+                        int flags = (int) regs.SYSCALL_ARG4;
+                        if (prot & PROT_READ && prot & PROT_WRITE &&
+                            flags & MAP_PRIVATE && flags & MAP_ANONYMOUS
+                        ) {
+                            show_backtrace++;
+                            printf("new anonymous mmap of %d bytes at %p\n", len, addr);
+                        }
+                    }
+                }
+            }
         }
 
-        if (state) {
-            // copy args on syscall entry
-            struct user_regs_struct regs;
-            ptrace(PTRACE_GETREGS, pid, NULL, &regs);
-            interesting = (regs.SYSCALL_NUM == SYS_brk) || 0;
-            //printf("syscall %ld: %s\n", regs.SYSCALL_NUM, x86_64_syscalls[regs.SYSCALL_NUM]);
-        }
-        else if (interesting) {
-            // display backtrace
+        if (show_backtrace) {
             unw_word_t ip, sp, start_ip = 0, off;
             int n = 0, ret;
-            unw_proc_info_t pi;
             unw_cursor_t c;
             char buf[512];
             size_t len;
@@ -81,7 +132,6 @@ main(int argc, char** argv)
             ret = unw_init_remote(&c, as, ui);
             if (ret < 0)
                 PANIC("unw_init_remote() failed: ret=%d\n", ret);
-
             do {
                 if ((ret = unw_get_reg(&c, UNW_REG_IP, &ip)) < 0
                         || (ret = unw_get_reg(&c, UNW_REG_SP, &sp)) < 0)
@@ -91,7 +141,7 @@ main(int argc, char** argv)
                     start_ip = ip;
 
                 buf[0] = '\0';
-                unw_get_proc_name(&c, buf, sizeof (buf), &off);
+                unw_get_proc_name(&c, buf, sizeof(buf), &off);
 
                 if (off) {
                     len = strlen(buf);
@@ -101,39 +151,36 @@ main(int argc, char** argv)
                 }
                 printf("%016lx %-32s (sp=%016lx)\n", (long) ip, buf, (long) sp);
 
-                if ((ret = unw_get_proc_info(&c, &pi)) < 0)
-                    PANIC("unw_get_proc_info(ip=0x%lx) failed: ret=%d\n", (long) ip, ret);
-                else
-                    printf("\tproc=%016lx-%016lx\n\thandler=%lx lsda=%lx\n",
-                           (long) pi.start_ip, (long) pi.end_ip,
-                           (long) pi.handler, (long) pi.lsda);
-
                 ret = unw_step(&c);
                 if (ret < 0) {
                     unw_get_reg(&c, UNW_REG_IP, &ip);
                     PANIC("FAILURE: unw_step() returned %d for ip=%lx (start ip=%lx)\n",
-                          ret, (long) ip, (long) start_ip);
+                        ret, (long) ip, (long) start_ip);
                 }
 
                 if (++n > 64) {
                     /* guard against bad unwind info in old libraries... */
                     PANIC("too deeply nested---assuming bogus unwind (start ip=%lx)\n",
-                          (long) start_ip);
-                    break;
+                        (long) start_ip);
+                }
+
+                if (image) {
+                    char addr2line[128];
+                    snprintf(addr2line, sizeof(addr2line), "addr2line -C -e %s -i %lx", image, ip);
+                    int ret = system(addr2line) < 0;
+                    if (ret < 0)
+                        WARN("addr2line failed: %s\n", strerror(errno));
                 }
             } while (ret > 0);
             printf("\n");
-
-            if (ret < 0)
-                PANIC("unwind failed with ret=%d\n", ret);
         }
 
-        if (ptrace(PTRACE_SYSCALL, pid, pending_sig, NULL) < 0)
+        if (ptrace(PTRACE_SYSCALL, pid, NULL, pending_sig) < 0)
             PANIC("PTRACE_SYSCALL: %s\n", strerror(errno));
     }
 
     CLEANUP:
-    ptrace(PTRACE_DETACH, pid, (char*) 1, 0);
+    ptrace(PTRACE_DETACH, pid, NULL, NULL);
     _UPT_destroy (ui);
     unw_destroy_addr_space(as);
 
